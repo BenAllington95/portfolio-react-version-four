5f3c0a98542a4d69b6ee07a87a7478122c067fe9
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-07-19 17:11:18 +0100

    Create react app, plus added new components, started scss

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './styles/main.css'
+
+// Components
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
