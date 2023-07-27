286db84d605948ff0b611856b50b7591d07f0ec2
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-07-27 17:39:07 +0100

    Add certificates section with a carousel

--- app/src/components/AboutSection.jsx
+++ app/src/components/AboutSection.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 
-const AboutSection = () => {
+const AboutSection = ({children}) => {
   return (
     <section className="about-section">
-      <div className="container">
         <div className="about-content"> 
-          <code>
-            <h2>about</h2>
-            <p> 
-                Hi, I'm Ben, a self-taught front-end developer passionate about coding and design. I've spent the last two years honing my HTML, CSS, JavaScript, and React skills by creating a selection of projects.
-            </p> 
-            <p> 
-                With strong problem-solving abilities, a keen eye for design, and valuable teamwork and perseverance lessons from sports, I can deliver high-quality work and fresh perspective.
-            </p> 
-        </code>
-        </div>
+          {children}
       </div>
     </section>
   );
--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, version } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import ScrollDownArrow from './ScrollDownArrow'
@@ -6,8 +6,32 @@ import ScrollDownArrow from './ScrollDownArrow'
 import HeroSection from './HeroSection';
 import AboutSection from './AboutSection';
 
+
+
+
 const Hero = () => {
   const carouselRef = useRef(null);
+  const carouselRefTwo = useRef(null);
+
+  const certificateImages = [
+    'https://images.unsplash.com/photo-1682685796063-d2604827f7b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80', // Replace this with the actual Unsplash link for your certificate image,
+    'https://images.unsplash.com/photo-1690205074022-560b3587cbc4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+  ];
+
+  const certificateViewerStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+  
+  const certificateImageStyles = {
+    maxWidth: '100%',
+    height: 'auto',
+    margin: '10px',
+  };
+  
+  
+
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -56,7 +80,7 @@ const renderCustomNextArrow = (onClickHandler, hasNext) => {
         infiniteLoop={true} 
         interval={25000} 
         showIndicators={false} 
-        emulateTouch={true}
+        emulateTouch={false}
         renderArrowPrev={renderCustomPrevArrow}
         renderArrowNext={renderCustomNextArrow}
       >
@@ -64,11 +88,49 @@ const renderCustomNextArrow = (onClickHandler, hasNext) => {
           <HeroSection />
         </div>
         <div className="hero-section">
-          <AboutSection />
+          <AboutSection>
+          <code>
+            <h2>about.ben</h2>
+            <p> 
+                Hi, I'm Ben, a self-taught front-end developer passionate about coding and design. I've spent the last two years honing my HTML, CSS, JavaScript, and React skills by creating a selection of projects.
+            </p> 
+            <p> 
+                With strong problem-solving abilities, a keen eye for design, and valuable teamwork and perseverance lessons from sports, I can deliver high-quality work and fresh perspective.
+            </p> 
+        </code>
+          </AboutSection>
         </div>
         <div className="hero-section">
-          <div className="page-component">
-          </div>
+          <AboutSection>
+          {/* <code>
+            <h2>certificates.ben</h2>
+          </code> */}
+
+          <Carousel 
+            ref={carouselRefTwo} 
+            showStatus={false} 
+            autoPlay={true} 
+            showThumbs={false} 
+            showIndicators={false} 
+            infiniteLoop={true}
+            interval={5000}
+            emulateTouch={true}
+            axis={'vertical'}
+            stopOnHover={true}
+            showArrows={false}
+          >
+                {certificateImages.map((certificate, index) => (
+                  <img
+                    key={index}
+                    src={certificate}
+                    alt={`Certificate ${index + 1}`}
+                    className="certificate-image"
+                  />
+                ))}
+          </Carousel>
+
+       
+          </AboutSection>
         </div>
       </Carousel>
     </section>
